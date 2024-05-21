3ab976305163899d6381d77972c4e8fd05542136
ifatimahali <star18salfa@gmail.com>
2024-05-21 18:44:55 +0300

    f

---
+++ main.js
@@ -0,0 +1,38 @@
+//1
+let x= 6
+let y = 9
+let result= x*y
+console.log("The area of the rectangle is "+ result);
+//2 
+let temp = 28
+let res = temp * 5/9 + 32
+console.log(temp + "°F is" + res+"°C" );
+//3
+let hour = 5
+let second = 54
+console.log(hour +  "hour" ,"=", second +  "Seconds");
+
+//4
+let age = 24
+let me= `my age is ${age}`
+console.log(me)
+
+//5
+var bill =10.25 + 3.99 + 7.15;
+var tip =bill * 0.15;
+var total = bill + tip;
+console.log(total);
+//6
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+console.log(madLib);
+
+//7 
+var firstName = "Fatimah";
+var interest = "Stars";
+var hobby = "watch the stars in sky night";
+var awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+console.log(awesomeMessage);
+
